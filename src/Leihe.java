80ec4bc3427d608d132df7583e731ee3377d8a09
pq <pq@brown>
2014-04-06 17:20:52 +0200

    Leihe (MitgliedsID entfernt, wird aus MitgliedObjekt gelesen)

--- src/Leihe.java
+++ src/Leihe.java
@@ -7,30 +7,23 @@ public class Leihe {
 	//Attribute
 	private int leihID;
 	private static int ANZAHL=1;
-	private int mitgliedID;
 	private Date datum;
-	private Mitglied besitzer;
+	private Mitglied mitglied;
 	private Medium medium;
 
 	//Konstruktor
 	
-	public Leihe(int mitgliedID, Date datum, Mitglied besitzer, Medium m) {
-		this.mitgliedID = mitgliedID;
+	public Leihe(int mitgliedID, Date datum, Mitglied mitglied, Medium m) {
 		this.datum = datum;
-		this.besitzer = besitzer;
+		this.mitglied = mitglied;
 	}
 
-	public int getMitgliedID() {
-		return mitgliedID;
-	}
-
-	
 	public Date getDatum() {
 		return datum;
 	}
 
-	public Mitglied getBesitzer() {
-		return besitzer;
+	public Mitglied getMitglied() {
+		return mitglied;
 	}
 	
 	public void setMedium(Medium m){
@@ -43,7 +36,7 @@ public class Leihe {
 	
 	public void druckeDaten(){
 		System.out.println("Leihe No. "+leihID);
-		System.out.println("MitgliedID: "+mitgliedID);
+		System.out.println("MitgliedID: "+mitglied.getMitgliedID());
 		System.out.println("Medium: "+medium.getTitel());
 	}
 	
